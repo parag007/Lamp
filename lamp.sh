1dadf8a2813ab6c179e347457c927e06fcf1054d
parag007 <paragchintoo007@gmail.com>
2018-07-18 20:19:03 -0400

    changes made in file

--- lamp.sh
+++ lamp.sh
@@ -1,8 +1,15 @@
 if [ "`lsb_release -is`" == "Ubuntu" ] || [ "`lsb_release -is`" == "Debian" ]
 then
+    
+    echo "Installing PHP 7"
     sudo apt-get install php
+    echo "________________"
     php -v
+    echo "________________"
+    echo "Installing Apache 2"
     sudo apt-get -y install apache2
+
+    echo "Installing percona-server 5"
     sudo apt-get install percona-server-server-5.6
 
 else
